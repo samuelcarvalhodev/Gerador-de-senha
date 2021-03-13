93fa2c132c6416f3e96151d75826397755ad5326
samuelcarvalhodev <samuelmacal.2015@gmail.com>
2021-03-13 15:08:15 -0300

    feat(generated password) Criação de app gerador de senha

--- App.js
+++ App.js
@@ -1,21 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+import Slider from '@react-native-community/slider';
+import Clipboard from 'expo-clipboard';
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [size, setSize] = useState(5);
+
+  function generatePass(){
+    let pass = '';
+    for(let i = 0, n = charset.length; i < size; i++ ){
+      pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    setPassword(pass);
+  }
+
+  function copyPass(){
+    Clipboard.setString(password)
+    alert('Senha Copiada com Sucesso!')
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <View style={styles.container}>
+     <Image
+        source={require('./src/assets/logo.png')}
+        style={styles.logo}
+     />
+     <Text style={styles.title}>{size} Caracteres</Text>
+    <View style={styles.area}> 
+      <Slider
+        style={{ height:50}}
+        minimumValue={5}
+        maximumValue={15}
+        minimumTrackTintColor="#ff0000"
+        maximumTrackTintColor="#000"
+        value={size}
+        onValueChange={(valor) => setSize(valor.toFixed(0)) }
+
+      />
     </View>
+
+    <TouchableOpacity 
+      style={styles.button} 
+      onPress={generatePass}
+    >
+      <Text style={styles.buttonText}>Gerar Senha</Text>
+    </TouchableOpacity>
+
+    {password !== '' && (
+      <View style={styles.area}>
+        <Text style={styles.password} onLongPress={copyPass}>{password}</Text>
+      </View>
+    )}
+   </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F3F3FE'
   },
-});
+  logo:{
+    marginBottom: 60
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  area:{
+    marginTop: 15,
+    marginBottom: 15,
+    width: '80%',
+    borderRadius: 7
+
+  },
+  button:{  
+    backgroundColor: '#FFA200',
+    width: '80%',
+    height: 50,
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius: 7,
+    marginBottom: 25
+
+  },
+
+  buttonText:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+
+  password:{
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20,
+  }
+})
